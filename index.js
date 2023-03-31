2c1d0800f2bec266395985ca713cf40ab90c4175
himanshu2k23 <himanshudubey2k23@gmail.com>
2023-03-31 12:56:56 +0530

    working

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
+const { v4: uuid } = require('uuid');
+const path = require('path');
 
+const comments=[
+    {
+        id:uuid(),
+        username: "Jhon",
+        comment:"I have a dream and a hope it comes true"
+    },
+    {
+        id:uuid(),
+        username: "Doe",
+        comment: "I wish that the sky up abova"
+    }
+];
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set('view engine','ejs');
+// app.set('view',path.join(__dirname,'views'));     
+
+
+app.get('/comments',(req,res)=>{
+    res.render(path.join(__dirname,'views/comments.ejs'),{ comments });
+});
+
+app.listen(3000, () => {
+    console.log("ON PORT 3000!")
+})
